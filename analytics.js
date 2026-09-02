98a9125da4f542d00592c495d604769bc5fb7e45
Georgii Khoruzhevskii <khoruzhevskiigeorgii@gmail.com>
2026-09-02 04:17:40 +0200

    Set Cloudflare Web Analytics token

--- analytics.js
+++ analytics.js
@@ -1,5 +1,5 @@
 (() => {
-  const CLOUDFLARE_WEB_ANALYTICS_TOKEN = '';
+  const CLOUDFLARE_WEB_ANALYTICS_TOKEN = '74877530d8b04bac8b1771a4d1748e63';
   if (!CLOUDFLARE_WEB_ANALYTICS_TOKEN) return;
   if (document.querySelector('script[data-cf-beacon]')) return;
 
